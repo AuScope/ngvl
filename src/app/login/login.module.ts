18f027b991ec3aff781a1f9d4c3070de8abf141c
Geoffrey Squire <squ022@triplewood-bm.nexus.csiro.au>
2018-01-09 15:42:50 +1100

    Include TranslateModule in login module.

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 
 @NgModule({
-    imports: [CommonModule, LoginRoutingModule],
+    imports: [CommonModule, LoginRoutingModule, TranslateModule],
     declarations: [LoginComponent]
 })
 export class LoginModule {}

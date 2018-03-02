252dbc91e5891bd29e07f7763f81041440d3f84b
Stuart Woodman <stuart.woodman@csiro.au>
2018-03-02 16:27:56 +1100

    Extend the map zoom component to have +/- buttons to allow zooming without a mousewheel.

--- src/app/layout/datasets/openlayermap/controls/olmap.zoom.component.ts
+++ src/app/layout/datasets/openlayermap/controls/olmap.zoom.component.ts
@@ -17,17 +17,25 @@ export class OlMapZoomComponent {
   /**
    * toggle on zoom to zoom into bbox
    */
-  public zoomClick() {
+  public zoomClick(): void {
     this.buttonText = 'Click on Map';
     this.olMapService.drawBound().subscribe((vector) => {
       const features = vector.getSource().getFeatures();
       const me = this;
       // Go through this array and get coordinates of their geometry.
       features.forEach(function(feature) {
-        me.buttonText = 'Magnify';
+        me.buttonText = 'Zoom';
         me.olMapService.fitView(feature.getGeometry().getExtent());
       });
 
     });
+  }
+
+  public zoomIn(): void {
+    this.olMapService.zoomMapIn();
+  }
+
+  public zoomOut(): void {
+    this.olMapService.zoomMapOut();
   }
 }
--- src/app/layout/datasets/openlayermap/olmap.component.ts
+++ src/app/layout/datasets/openlayermap/olmap.component.ts
@@ -1,5 +1,7 @@
 import { OlMapObject } from 'portal-core-ui/service/openlayermap/ol-map-object';
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import olControlZoom from 'ol/control/zoom';
+
 
 /*
  * Note: The hight style is important or the map won't appear on first load
@@ -7,7 +9,7 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-ol-map',
   template: `
-    <div #mapElement id="map" class="height-full width-full" style="height:calc(100vh - 200px);"> </div>
+    <div #mapElement id="map" class="height-full width-full" style="height:calc(100vh - 200px);"></div>
     `
   // The "#" (template reference variable) matters to access the map element with the ViewChild decorator!
 })
@@ -17,13 +19,14 @@ export class OlMapComponent implements AfterViewInit {
   @ViewChild('mapElement') mapElement: ElementRef;
 
 
-
   constructor(public olMapObject: OlMapObject) { }
 
 
   // After view init the map target can be set!
   ngAfterViewInit() {
     this.olMapObject.getMap().setTarget(this.mapElement.nativeElement.id);
+    const zoom: olControlZoom = new olControlZoom();
+    this.olMapObject.addControlToMap(zoom);
   }
 
 }

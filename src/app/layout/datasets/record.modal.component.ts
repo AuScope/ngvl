a18beb785344fef43e7cb9a7679e1dcdcbb60a45
Stuart Woodman <stuart.woodman@csiro.au>
2018-03-05 12:06:57 +1100

    CSW record information now displays online resource information (NCSS, WCS, WMS, and WWW).

--- src/app/layout/datasets/record.modal.component.ts
+++ src/app/layout/datasets/record.modal.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Constants } from 'portal-core-ui/utility/constants.service';
+import { CSWRecordModel } from 'portal-core-ui/model/data/cswrecord.model';
+import { OnlineResourceModel } from 'portal-core-ui/model/data/onlineresource.model';
 
 
 @Component({
@@ -11,5 +14,43 @@ export class RecordModalContent {
 
     @Input() record: any;
 
+    // Service submenu dropdowns
+    ncssExpanded: boolean = true;
+    wcsExpanded: boolean = true;
+    wmsExpanded: boolean = true;
+    wwwExpanded: boolean = true;
+
+
     constructor(public activeModal: NgbActiveModal) { }
+
+
+    /**
+     * 
+     * @param cswRecord 
+     */
+    public getNetCDFServices(cswRecord: CSWRecordModel): OnlineResourceModel[] {
+        let serviceList: OnlineResourceModel[] = [];
+        for (const onlineResource of cswRecord.onlineResources) {
+            if (onlineResource.type === Constants.resourceType.NCSS) {
+                let res: OnlineResourceModel = onlineResource;
+                res.cswRecord = cswRecord;
+                serviceList.push(res);
+            }
+        }
+        return serviceList;
+    }
+
+
+    public getOnlineResources(cswRecord: CSWRecordModel, resourceType: string): OnlineResourceModel[] {
+        let serviceList: OnlineResourceModel[] = [];
+        for (const onlineResource of cswRecord.onlineResources) {
+            if (onlineResource.type === resourceType) {
+                let res: OnlineResourceModel = onlineResource;
+                //res.cswRecord = cswRecord;
+                serviceList.push(res);
+            }
+        }
+        return serviceList;
+    }
+
 }

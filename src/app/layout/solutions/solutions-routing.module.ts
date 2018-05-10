048af719988626e1c483b963bbff3fc9b21699e5
Geoffrey Squire <squ022@triplewood-bm.nexus.csiro.au>
2018-05-10 16:15:56 +1000

    WIP on solution selection

--- src/app/layout/solutions/solutions-routing.module.ts
+++ src/app/layout/solutions/solutions-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from '../../shared';
 import { SolutionsComponent } from './solutions.component';
 
-const routes: Routes = [{ path: '', component: SolutionsComponent }];
+const routes: Routes = [{ path: '', component: SolutionsComponent, canActivate: [AuthGuard]  }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/shared/guard/auth.guard.ts
+++ src/app/shared/guard/auth.guard.ts
@@ -2,12 +2,19 @@ import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
 import { Router } from '@angular/router';
 
+import { UserStateService } from '../services/user-state.service';
+import { ANONYMOUS_USER, User } from '../modules/vgl/models';
+
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router) {}
+  private currentUser: User;
+
+  constructor(private router: Router,
+              private userStateService: UserStateService) {}
 
   canActivate() {
-    if (localStorage.getItem('isLoggedin')) {
+    if (localStorage.getItem('isLoggedin') &&
+        this.currentUser !== ANONYMOUS_USER) {
       return true;
     }
 

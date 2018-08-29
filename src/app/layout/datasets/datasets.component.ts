bb25400f4dde77ecb7209894ea303731696b85cc
Stuart Woodman <stuart.woodman@csiro.au>
2018-08-29 10:50:10 +1000

    * Add Layer button adjusted to better check if a layer can be added to the map. Current checks are:   1. CSWRecord has on online resource.   2. CSWRecord has at least one geographic element displayed.   3. CSWRecord is not already on the map.   4. At least one online resource is of a valid type supported by current version of portal-core-ui (WMS, WFS, CSW, WWW). * Minor adjustment to job submission page to use a router link instead of a page link (to /data when there are no datasets) to prevent the page reloading.

--- src/app/layout/datasets/datasets.component.ts
+++ src/app/layout/datasets/datasets.component.ts
@@ -28,7 +28,6 @@ export class DatasetsComponent implements OnInit, AfterViewChecked {
 
     // Search results   
     cswSearchResults: CSWRecordModel[] = [];
-    //selectedCSWRecord = null;
     recordsLoading = false;
 
     //BookMarks    

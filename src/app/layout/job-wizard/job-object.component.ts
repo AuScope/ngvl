e026eb7134ac19b50a1ba48b08aa1b5941e642ec
Geoffrey Squire <geoffrey.squire@csiro.au>
2018-09-06 15:19:42 +1000

    Fix toolbox/vmid loading

--- src/app/layout/job-wizard/job-object.component.ts
+++ src/app/layout/job-wizard/job-object.component.ts
@@ -48,11 +48,11 @@ export class JobObjectComponent implements OnDestroy, OnInit {
       this.vgl.getComputeServices(this.job.id).subscribe(
         computeServices => {
           this.computeProviders = computeServices;
+
+          // Load toolboxes if the user had already selected one
+          this.computeProviderChanged(this.job.computeServiceId);
         }
       );
-
-      // Load toolboxes if the user had already selected one
-      this.computeProviderChanged(this.job.computeServiceId);
     });
   }
 
@@ -62,60 +62,60 @@ export class JobObjectComponent implements OnDestroy, OnInit {
     }
   }
 
-    /**
-     * Make the Job available to the wizard
-     */
-    public getJobObject(): Job {
-        // Disregard walltime value present in UI if User has chosen not to use it
-        if(!this.useWalltime) {
-            this.job.walltime = undefined;
-        }
-        return this.job;
+  /**
+   * Make the Job available to the wizard
+   */
+  public getJobObject(): Job {
+    // Disregard walltime value present in UI if User has chosen not to use it
+    if(!this.useWalltime) {
+      this.job.walltime = undefined;
     }
 
+    return this.job;
+  }
 
-    /**
-     * When compute provider is changed, load toolboxes
-     *
-     * @param computeServiceId the new compute provider id.
-     */
-    public computeProviderChanged(computeServiceId): void {
-      if (computeServiceId && computeServiceId !== "") {
-        // If we have a list of solutions use that, otherwise use the job id if
-        // one has been assigned. If neither is available, don't load any
-        // toolboxes yet.
-        const solutions = this.userStateService.getSolutionsCart().map(s => s['@id']);
-        this.vgl.getMachineImages(computeServiceId, solutions, this.job.id)
-          .subscribe(images => this.toolboxes = images);
 
-        // Load the resources if User had already selected one
-        const computeVmId = this.job.computeVmId;
-        if(computeVmId && computeVmId !== "") {
-          this.jobsService.getComputeTypes(computeServiceId, computeVmId)
-            .subscribe(computeTypes => this.resources = computeTypes);
-        }
+  /**
+   * When compute provider is changed, load toolboxes
+   *
+   * @param computeServiceId the new compute provider id.
+   */
+  public computeProviderChanged(computeServiceId): void {
+    if (computeServiceId && computeServiceId !== "") {
+      // If we have a list of solutions use that, otherwise use the job id if
+      // one has been assigned. If neither is available, don't load any
+      // toolboxes yet.
+      const solutions = this.userStateService.getSolutionsCart().map(s => s['@id']);
+      this.vgl.getMachineImages(computeServiceId, solutions, this.job.id)
+        .subscribe(images => {
+          this.toolboxes = images;
+
+          // Select the first image in the list by default, and update resources accordingly.
+          if (this.toolboxes.length > 0) {
+            const computeVmId = this.toolboxes[0].imageId;
+            this.job.computeVmId = computeVmId;
+            this.toolboxChanged(computeVmId);
+          }
+        });
+    }
+  }
+
+  /**
+   * When toolbox is changed and we're using a cloud compute provider, reload
+   * the available cloud resources.
+   *
+   * @param event the toolbox select change event
+   */
+  public toolboxChanged(toolbox): void {
+    if (this.isCloudProvider(this.job.computeServiceId)) {
+      if(toolbox && toolbox !== "") {
+        this.vgl.getComputeTypes(this.job.computeServiceId, toolbox)
+          .subscribe(computeTypes => {
+            this.resources = computeTypes;
+          });
       }
     }
-
-    /**
-     * When toolbox is changed, load resources
-     *
-     * @param event the toolbox select change event
-     */
-    public toolboxChanged(toolbox): void {
-        // Load compute types (Resources)
-        if(toolbox && toolbox !== "") {
-            this.userStateService.job.subscribe(
-                job => {
-                    this.jobsService.getComputeTypes(job.computeServiceId, toolbox).subscribe(
-                        computeTypes => {
-                            this.resources = computeTypes;
-                        }
-                    );
-                }
-            );
-        }
-    }
+  }
 
   public isCloudProvider(providerId: string): boolean {
     return this.vgl.isCloudProvider(providerId);

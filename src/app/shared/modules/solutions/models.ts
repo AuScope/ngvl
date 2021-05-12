a241770fe5bf924c7045b7ed0f67a5a140738077
Geoffrey Squire <geoffrey.squire@csiro.au>
2021-05-12 11:34:22 +1000

    Support date-typed solution variables

--- src/app/shared/modules/solutions/models.ts
+++ src/app/shared/modules/solutions/models.ts
@@ -110,6 +110,10 @@ export class BooleanBinding extends InputBinding<boolean> {
   controlType = 'checkbox';
 }
 
+export class DateBinding extends InputBinding<string> {
+  controlType = 'date';
+}
+
 export function create_var_binding(
   variable: Variable,
   options = {}
@@ -129,6 +133,12 @@ export function create_var_binding(
     opts.values = variable.values;
     return variable.type === 'string' ? new OptionsBinding<string>(opts) : new OptionsBinding<number>(opts);
   } else {
+    // Check for annotations first, then fall back on simple type-based
+    // selection.
+    if (variable.tags.includes('datetime') || variable.tags.includes('iso8601')) {
+      return new DateBinding(opts);
+    }
+
     switch (variable.type) {
       case 'int':
         return new NumberEntryBinding({
